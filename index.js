feb7442d01952a14e876b5555decfaa839b9a902
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-03 02:41:43 +0600

    add mongo db or get books data

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv').config()
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zat3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+try{
+    await client.connect();
+    const booksCollection = client.db("bookInventory").collection("books");
+    console.log(booksCollection)
+    app.get('/books', async (req,res)=>{
+        const query = {};
+        const cursor = booksCollection.find(query);
+        const books = await cursor.toArray();
+        res.send(books)
+    })
+
+}
+finally{
+    // client.close();
+}
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
