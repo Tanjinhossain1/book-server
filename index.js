09553365ce843d734f8ec6afa74b59ee78432886
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-03 03:12:29 +0600

    create single get data api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config()
@@ -15,22 +15,27 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zat3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-try{
-    await client.connect();
-    const booksCollection = client.db("bookInventory").collection("books");
-    console.log(booksCollection)
-    app.get('/books', async (req,res)=>{
-        const query = {};
-        const cursor = booksCollection.find(query);
-        const books = await cursor.toArray();
-        res.send(books)
-    })
-
-}
-finally{
-    // client.close();
-}
+async function run() {
+    try {
+        await client.connect();
+        const booksCollection = client.db("bookInventory").collection("books");
+        console.log(booksCollection)
+        app.get('/books', async (req, res) => {
+            const query = {};
+            const cursor = booksCollection.find(query);
+            const books = await cursor.toArray();
+            res.send(books)
+        })
+        app.get('/books/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const book = await booksCollection.findOne(query);
+            res.send(book)
+        })
+    }
+    finally {
+        // client.close();
+    }
 }
 run().catch(console.dir)
 
